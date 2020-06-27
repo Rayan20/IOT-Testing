c044abb040d567314b35c66074846db7d774d603
Yang, Richard <Richard.Yang@pearson.com>
2020-06-27 17:33:07 -0500

    switching to WAN ip not LAN ip

--- environment.config.js
+++ environment.config.js
@@ -6,8 +6,8 @@ module.exports = {
 
     },
     iot:{
-        server: "http://192.168.1.128"
-        //   server: "http://63.152.45.86"
+       // server: "http://192.168.1.128"
+        server: "http://63.152.45.86"
         // Riley's server: "50.80.241.185"
     },
 
